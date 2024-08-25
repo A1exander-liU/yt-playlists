dfc50d53ee5e175bb15a4df21679add59fd0a9cb
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-25 13:38:06 -0700

    chore: rearrange methods

--- api/playlists/playlist.go
+++ api/playlists/playlist.go
@@ -24,6 +24,25 @@ func listPlaylists(yt *youtube.Service, part []string, nextPage string) (*youtub
 	return res, nil
 }
 
+func insertPlaylist(yt *youtube.Service, name string, description string, status string) (*youtube.Playlist, error) {
+	if status != "public" && status != "private" && status != "unlisted" {
+		return nil, fmt.Errorf("%v is not a valid status: Must be one of 'private', 'public', or 'unlisted'", status)
+	}
+	newPlaylistStatus := &youtube.PlaylistStatus{PrivacyStatus: status}
+	newPlaylist := &youtube.Playlist{Snippet: &youtube.PlaylistSnippet{Title: name, Description: description}, Status: newPlaylistStatus}
+	req := yt.Playlists.Insert([]string{"snippet,status"}, newPlaylist)
+
+	res, err := req.Do()
+	return res, err
+}
+
+func deletePlaylist(yt *youtube.Service, playlistId string) error {
+	req := yt.Playlists.Delete(playlistId)
+
+	err := req.Do()
+	return err
+}
+
 func (playlistService *PlaylistService) List(part []string) (*youtube.PlaylistListResponse, error) {
 	res, err := listPlaylists(playlistService.yt, part, "")
 	playlists := []*youtube.Playlist{}
@@ -44,28 +63,9 @@ func (playlistService *PlaylistService) List(part []string) (*youtube.PlaylistLi
 	return res, nil
 }
 
-func insertPlaylist(yt *youtube.Service, name string, description string, status string) (*youtube.Playlist, error) {
-	if status != "public" && status != "private" && status != "unlisted" {
-		return nil, fmt.Errorf("%v is not a valid staus: Must be one of 'private', 'public', or 'unlisted'", status)
-	}
-	newPlaylistStatus := &youtube.PlaylistStatus{PrivacyStatus: status}
-	newPlaylist := &youtube.Playlist{Snippet: &youtube.PlaylistSnippet{Title: name, Description: description}, Status: newPlaylistStatus}
-	req := yt.Playlists.Insert([]string{"snippet,status"}, newPlaylist)
-
-	res, err := req.Do()
-	return res, err
-}
-
 func (playlistService *PlaylistService) Insert(name string, description string, status string) (*youtube.Playlist, error) {
 	res, err := insertPlaylist(playlistService.yt, name, description, status)
 	return res, err
-}
-
-func deletePlaylist(yt *youtube.Service, playlistId string) error {
-	req := yt.Playlists.Delete(playlistId)
-
-	err := req.Do()
-	return err
 }
 
 func (playlistService *PlaylistService) Delete(playlistId string) error {
