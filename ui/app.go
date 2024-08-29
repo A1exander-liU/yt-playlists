4c89d7ca983992b9f1e62c5c562b7339118f2096
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-29 15:52:01 -0700

    feat: layout components

--- ui/app.go
+++ ui/app.go
@@ -1,15 +1,19 @@
 package ui
 
 import (
+	"os"
+
 	"example.com/demo/api"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 type App struct {
 	*tview.Application
-	api   *api.ApiService
-	pages *tview.Pages
-	views map[string]tview.Primitive
+	api    *api.ApiService
+	pages  *tview.Pages
+	views  map[string]tview.Primitive
+	modals map[string]bool
 }
 
 func New() *App {
@@ -18,6 +22,7 @@ func New() *App {
 		api:         api.New(),
 		pages:       tview.NewPages(),
 		views:       make(map[string]tview.Primitive),
+		modals:      make(map[string]bool),
 	}
 
 	return &app
@@ -27,6 +32,9 @@ func (a *App) Init() {
 	// create views
 	playlists := NewPlaylists(a)
 	videos := NewVideos(a)
+	a.views["Playlists"] = playlists.view
+	a.views["Videos"] = videos.view
+
 	main := tview.NewFlex().
 		AddItem(playlists.view, 0, 1, true).
 		AddItem(videos.view, 0, 3, false)
@@ -38,9 +46,63 @@ func (a *App) Init() {
 
 	a.pages.AddPage("Main", main, true, true)
 
+	main.SetMouseCapture(func(action tview.MouseAction, event *tcell.EventMouse) (tview.MouseAction, *tcell.EventMouse) {
+		if a.pages.HasPage("Modal") {
+			return action, nil
+		}
+		return action, event
+	})
+
+	a.SetInputCapture(a.keyboard)
 	a.Application.SetRoot(a.pages, true).EnableMouse(true)
+}
+
+func (a *App) DisplayModal(p tview.Primitive, name string, onCancel func()) {
+	a.pages.AddPage(name, Modal(p, onCancel, 40, 20), true, true)
+	a.modals[name] = true
+}
+
+func (a *App) CloseModal(name string) {
+	a.pages.RemovePage(name)
+	delete(a.modals, name)
+}
+
+func (a *App) ModalActive() bool {
+	return len(a.modals) > 0
 }
 
 func (a *App) Run() error {
 	return a.Application.Run()
+}
+
+// Helper
+
+func (a *App) keyboard(event *tcell.EventKey) *tcell.EventKey {
+	if event.Rune() == 'q' {
+		a.Stop()
+		os.Exit(0)
+	}
+
+	if event.Rune() == '?' {
+		a.toggleHelp()
+	}
+
+	if !a.ModalActive() && event.Key() == tcell.KeyTAB {
+		if a.views["Playlists"].HasFocus() {
+			a.SetFocus(a.views["Videos"])
+		} else {
+			a.SetFocus(a.views["Playlists"])
+		}
+	}
+
+	return event
+}
+
+func (a *App) toggleHelp() {
+	if a.pages.HasPage("Help") {
+		a.CloseModal("Help")
+	} else {
+		view := ListModal("Help", 40, 20, func() { a.CloseModal("Help") })
+		a.DisplayModal(view, "Help", func() { a.CloseModal("Help") })
+	}
 }
