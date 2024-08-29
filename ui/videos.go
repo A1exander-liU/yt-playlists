653cc0daad6529249ebc20d0312b200fdf2c469b
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-29 15:51:22 -0700

    docs: add to videos.go

--- ui/videos.go
+++ ui/videos.go
@@ -1,31 +1,33 @@
 package ui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"google.golang.org/api/youtube/v3"
 )
 
+// Component to display videos of a playlist
 type Video struct {
-	app    *App
-	view   *tview.List
-	videos []*youtube.PlaylistItem
+	app            *App
+	view           *tview.List
+	videos         []*youtube.PlaylistItem
+	selectedVideos map[int]*youtube.PlaylistItem
 }
 
+// Creates a new Video component
 func NewVideos(a *App) *Video {
 	video := Video{
-		app:    a,
-		view:   tview.NewList(),
-		videos: []*youtube.PlaylistItem{},
+		app:            a,
+		view:           tview.NewList(),
+		videos:         []*youtube.PlaylistItem{},
+		selectedVideos: make(map[int]*youtube.PlaylistItem),
 	}
-	video.Init()
+	video.init()
 
 	return &video
 }
 
-func (v *Video) Init() {
-	v.view.SetHighlightFullLine(true).ShowSecondaryText(true).SetBorder(true).SetTitle("Videos").SetBorderPadding(0, 0, 1, 1)
-}
-
+// Callback when playlist was selected
 func (v *Video) PlaylistSelected(playlistId string) {
 	go func() {
 		videos, err := v.app.api.PlaylistItems.List(playlistId, []string{"snippet"})
@@ -38,10 +40,34 @@ func (v *Video) PlaylistSelected(playlistId string) {
 }
 
 // Helpers
-func (p *Video) refreshItems() {
-	p.view.Clear()
 
-	for _, video := range p.videos {
-		p.view.AddItem(video.Snippet.Title, video.Snippet.VideoOwnerChannelTitle, 0, nil)
+// Initializes the component
+func (v *Video) init() {
+	v.view.SetHighlightFullLine(true).ShowSecondaryText(true).SetWrapAround(false).SetBorder(true).SetTitle("Videos").SetBorderPadding(0, 0, 1, 1)
+	v.view.SetInputCapture(v.keyboard)
+}
+
+// Redraws the video items
+func (v *Video) refreshItems() {
+	v.view.Clear()
+
+	for _, video := range v.videos {
+		v.view.AddItem(video.Snippet.Title, video.Snippet.VideoOwnerChannelTitle, 0, nil)
 	}
+}
+
+// Handles keyboard input
+func (v *Video) keyboard(event *tcell.EventKey) *tcell.EventKey {
+	if event.Key() == tcell.KeyTAB {
+		return nil
+	}
+
+	switch event.Rune() {
+	case 'j':
+		return tcell.NewEventKey(tcell.KeyDown, 0, tcell.ModNone)
+	case 'k':
+		return tcell.NewEventKey(tcell.KeyUp, 0, tcell.ModNone)
+	}
+
+	return event
 }
