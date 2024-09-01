74b70ac27237a5c30b534a94bb351cd45e4038d6
A1exander-liU <alexanderliu2016@gmail.com>
2024-09-01 14:32:57 -0700

    feat: create modals

--- ui/modal.go
+++ ui/modal.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"google.golang.org/api/youtube/v3"
 )
 
 func Modal(p tview.Primitive, onCancel func(), width, height int) tview.Primitive {
@@ -31,4 +32,45 @@ func Modal(p tview.Primitive, onCancel func(), width, height int) tview.Primitiv
 func ListModal(title string, width, height int, onCancel func()) tview.Primitive {
 	list := tview.NewList().SetHighlightFullLine(true).SetTitle(title).SetBorder(true)
 	return Modal(list, onCancel, width, height)
+}
+
+func DeleteDialog(message string, confirm func(), cancel func()) *tview.Modal {
+	dialog := tview.NewModal().
+		SetText(message).
+		AddButtons([]string{"No", "Yes"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			switch buttonLabel {
+			case "No":
+				cancel()
+			case "Yes":
+				confirm()
+				cancel()
+			}
+		})
+
+	dialog.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Rune() {
+		case ' ':
+			return tcell.NewEventKey(tcell.KeyEnter, 0, tcell.ModNone)
+		}
+
+		return event
+	})
+
+	return dialog
+}
+
+// 2 modals on for list, one fir onfirm
+func SelectPlaylistDialog(items []*youtube.Playlist, selected func(*youtube.Playlist), cancel func()) tview.Primitive {
+	list := tview.NewList().
+		SetHighlightFullLine(true).
+		ShowSecondaryText(false)
+
+	for _, item := range items {
+		list.AddItem(item.Snippet.Title, "", 0, func() { selected(item) })
+	}
+
+	playlistModal := Modal(list, cancel, 40, 20)
+
+	return playlistModal
 }
